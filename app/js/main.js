4ad4801717e4315da514e08fc10ea7180b9d25f8
KonstantinSin <konstsin0909@gmail.com>
2022-04-27 17:10:47 +0300

    added adptive some section

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,49 @@
 
 $(function(){
+  const element = document.querySelector('.js-choice');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+  });
+  });
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
 // кнопка еще подкасты
 $('.podcast__link').on('click', function(){
     $('.podcast-more').toggleClass('podcast-more--active');
   });
   // селект стайл
-  const element = document.querySelector('.js-choice');
-  const choices = new Choices(element, {
-    searchEnabled: false,
-  });
+
   $('.guest-block__title').click(function(event){
     $(this).toggleClass('active').next().slideToggle(300);
   });
-
+// скролл из нав бара
   $(".menu a").on("click", function (event) {
 
 		event.preventDefault();
 		var id  = $(this).attr('href'),
 			top = $(id).offset().top;
 		$('body,html').animate({scrollTop: top}, 1500);
-	});
-});
+  });
+
+
+
+// слайдер жанры
+$('.playlist__styles--mobile').slick({
+  // dots: false,
+  // arrows: false,
+  // slidesToShow: 1,
+  // slidesToScroll: 1,
+  // loop: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  arrows: false,
+}); 
+// радио кнопка жанры
 function clickRadio(el) {
   var siblings = document.querySelectorAll("input[type='radio'][name='" + el.name + "']");
   for (var i = 0; i < siblings.length; i++) {
@@ -31,5 +54,6 @@ function clickRadio(el) {
     el.checked = false;
   el.oldChecked = el.checked;
 }
+
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,8 @@ function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-        'node_modules/Choices-master/public/assets/scripts/choices.js',
+        'node_modules/slick-slider/slick/slick.js',
+        'node_modules/choices.js/public/assets/scripts/choices.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
