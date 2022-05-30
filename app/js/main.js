0c09e5f7c321c6e09e16636209e3c97370de492a
KonstantinSin <konstsin0909@gmail.com>
2022-05-30 20:15:55 +0300

    pre pp

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,57 @@
 
+// кастом селект передачи
 $(function(){
   const element = document.querySelector('.js-choice');
   const choices = new Choices(element, {
     searchEnabled: false,
   });
   });
-  $('.menu__btn').on('click', function(){
+
+// бургер меню
+  $('.menu-btn').on('click', function(){
     $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu-btn').click(function(){
+    $(this).toggleClass('open');
   });
 
 // кнопка еще подкасты
 $('.podcast__link').on('click', function(){
     $('.podcast-more').toggleClass('podcast-more--active');
   });
-  // селект стайл
 
+
+  $(document).ready(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 470) $(".podcast-mobile").addClass("podcast-more");
+    else $(".podcast-mobile").removeClass("podcast-more");
+  
+    $(window).resize(function() {
+      var windowWidth = $(window).width();
+      if (windowWidth < 470) $(".podcast-mobile").addClass("podcast-more");
+      else $(".podcast-mobile").removeClass("podcast-more");
+    });
+  });
+  
+
+// что в эфире
+  $('.header__bottom-mobile-box').click(function(event){
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
+
+// гости
   $('.guest-block__title').click(function(event){
     $(this).toggleClass('active').next().slideToggle(300);
+  });
+
+  $('.guest-block__link').on('click', function(e){
+    e.preventDefault();
+    $('.guest-block__link').removeClass('guest-block__link--active');
+    $(this).addClass('guest-block__link--active');
+
+    $('.guest__bio').removeClass('guest__bio--active');
+    $($(this).attr('href')).addClass('guest__bio--active');
   });
 // скролл из нав бара
   $(".menu a").on("click", function (event) {
@@ -27,15 +62,8 @@ $('.podcast__link').on('click', function(){
 		$('body,html').animate({scrollTop: top}, 1500);
   });
 
-
-
 // слайдер жанры
 $('.playlist__styles--mobile').slick({
-  // dots: false,
-  // arrows: false,
-  // slidesToShow: 1,
-  // slidesToScroll: 1,
-  // loop: true,
   infinite: true,
   speed: 300,
   slidesToShow: 1,
@@ -43,6 +71,7 @@ $('.playlist__styles--mobile').slick({
   variableWidth: true,
   arrows: false,
 }); 
+
 // радио кнопка жанры
 function clickRadio(el) {
   var siblings = document.querySelectorAll("input[type='radio'][name='" + el.name + "']");
@@ -55,5 +84,38 @@ function clickRadio(el) {
   el.oldChecked = el.checked;
 }
 
+// валидация формы
+const validation = new JustValidate('#form' , {
+  errorFieldCssClass: 'is-invalid',
+  errorLabelStyle: {
+    color: '#FF6F6F',
+  },
+}
+);
+validation
+.addField('#name', [
+  {
+    rule: 'minLength',
+    value: 3,
+    errorMessage: 'Ошибка',
+  },
+  {
+    rule: 'maxLength',
+    value: 30,
+    errorMessage: 'Ошибка',
+   
+  },
+])
+.addField('#email', [
+  {
+    rule: 'email',
+    errorMessage: 'Ошибка',
+  },
+]);
+    
+    
+
+  
 
 
+  
--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
         'node_modules/slick-slider/slick/slick.js',
+        'node_modules/just-validate/dist/just-validate.production.min.js',
         'node_modules/choices.js/public/assets/scripts/choices.js',
         'app/js/main.js'
     ])
